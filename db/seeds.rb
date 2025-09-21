beadc09efaa277018fac62fef23b7f9918c139b4
athanael gerard <pilotewpa@gmail.com>
2025-09-21 16:08:06 +0200

    project finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts 'Cleaning database...'
+Article.destroy_all
+puts 'All the article are destroyed'
+
+puts 'Creating 10 articles...'
+10.times do |n|
+  n = Article.new(title: Faker::Book.unique.title, content: Faker::Lorem.paragraphs(number: 5).join("\n\n"))
+  n.save
+  puts "New article #{n.title} created"
+end
+
+puts "All the articles are created"
